f0245b7a93d0c063a17fb39079baf9d86762dafa
JordonT3 <jordonthebananaman@gmail.com>
2025-04-15 10:39:10 -0500

    Add files via upload

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -1,12 +1,16 @@
-def count_vowels(text):
-    vowels = "aeiouAEIOU"
-    count = 0
-    for char in text:
-        if char in vowels:
-            count += 1
-    return count
+def count_each_vowel(text):
+    vowels = 'aeiou'
+    counts = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+    total = 0
+
+    for char in text.lower():
+        if char in counts:
+            counts[char] += 1
+            total += 1
+    for vowel, count in counts.items():
+        print(f"{vowel} = {count}")
+    print(f"Total vowels = {total}")
         
 
 user_input = input("Enter a sentance: ")
-vowel_count = count_vowels(user_input)
-print(f"There are {vowel_count} vowels in this text.")
+count_each_vowel(user_input)
